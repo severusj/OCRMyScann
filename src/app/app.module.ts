c88444ccf4a659f70283bb244ba3a0c9a3501dc0
Gabriel Vega <gvegag@miumg.edu.gt>
2020-05-23 22:06:22 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { WebcamModule } from 'ngx-webcam';
-
-
 import { AppComponent } from './app.component';
 import { OcrComponent } from './ocr/ocr.component';
 import { OcrbywebcamComponent } from './ocrbywebcam/ocrbywebcam.component';
